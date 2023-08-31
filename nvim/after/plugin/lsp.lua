72b3e510c3c15f249c87614ec7f4715b55747948
Tomasz Wester <111870494+tmwpwo@users.noreply.github.com>
2023-08-31 15:09:28 +0200

    adding formatting options

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -9,4 +9,28 @@ end)
 -- (Optional) Configure lua language server for neovim
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
+local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
+
+cmp.setup({
+	mapping = {
+		['<Tab>'] = cmp_action.luasnip_supertab(),
+		['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
+	}
+})
+
+lsp.format_mapping('gq', {
+	format_otps = {
+		async = false,
+		timeout_ms = 10000,
+	},
+	servers = {
+		['lua_ls'] = { 'lua' },
+		['rust_analyzer'] = { 'rust' },
+		['gopls'] = { 'go' },
+		['pyright'] = { 'python' },
+	}
+})
+
+
 lsp.setup()
